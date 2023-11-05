73ba76b883742c3172340a38ca4feb44248764c1
yadidev <yadinds123@gmail.com>
2023-11-04 23:21:27 -0400

    Agregando rutas modificadas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\MunicipalityController;
+use App\Http\Controllers\ProvinceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,8 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers'], function () {
+  Route::apiResource('provinces', ProvinceController::class);
+  Route::apiResource('municipalities', MunicipalityController::class);
+});
