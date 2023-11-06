f495fb8e3ab1fd208a32dea97b44a65ba3965357
yadidev <yadinds123@gmail.com>
2023-11-05 23:22:12 -0500

    Modificando endpoints

--- routes/api.php
+++ routes/api.php
@@ -23,10 +23,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers'], function () {
+  Route::get('/provincias-municipios', [ProvinceController::class, 'all']);
   Route::apiResource('provincias', ProvinceController::class);
   Route::apiResource('municipios', MunicipalityController::class);
-  Route::get('/all', function () {
-    $provinces = Province::all();
-    return new ProvinceCollection($provinces);
-  });
 });
