6da4e38f4ef3a1fb2a6b7601fffb3e9e8c7a7128
yadidev <yadinds123@gmail.com>
2023-11-09 15:15:09 -0500

    borrando datos innesesarios

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+
 
 Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers'], function () {
   /* Rutas de provincias */
