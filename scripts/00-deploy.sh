db58057f0dcf7d00f41f46b4b4519d13cd73af71
yadidev <yadinds123@gmail.com>
2023-11-09 00:02:32 -0500

    Agregando configuraciones para deploy

---
+++ scripts/00-deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "Running composer install..."
+composer install --no-dev --working-dir=/var/www/html
+
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Runing seed"
+php artisan db:seed
