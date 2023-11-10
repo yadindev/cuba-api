20d0a4c0fbe5ea417bbd8cee94e1d7bc4d1b9623
yadidev <yadinds123@gmail.com>
2023-11-10 00:02:59 -0500

    Agregando api-doc

--- app/Http/Controllers/MunicipalityController.php
+++ app/Http/Controllers/MunicipalityController.php
@@ -8,14 +8,38 @@ use App\Http\Requests\UpdateMunicipalityRequest;
 use App\Http\Resources\MunicipalityCollection;
 use App\Http\Resources\MunicipalityResource;
 
+/**
+ * @OA\Tag(
+ *     name="Municipios",
+ *     description="Datos de los municipios"
+ * )
+ */
 class MunicipalityController extends Controller
 {
     /**
-     * Mostrar Municipios
+     * Obtener  municipios
      * 
-     * Muestra solo los municipios con sus datos 
-     * 
-     * 
+     * Obtiene todos los datos de todos los municipios
+     * @OA\Get (
+     *     path="/municipios",
+     *     tags={"Municipios"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 type="array",
+     *                 property="data",
+     *                  @OA\Items(
+     *                      type="object",
+     *                      @OA\Property(property="id_municipio",type="number",example="1"),
+     *                      @OA\Property(property="nombre_municipio",type="string",example="Alquizar"),
+     *                      @OA\Property(property="es_cabecera_provincial",type="boolean",example="false"),
+     *                  ) 
+     *             )
+     *         )
+     *     )
+     * )
      */
     public function index()
     {
@@ -25,12 +49,60 @@ class MunicipalityController extends Controller
 
 
     /**
-     * Muestra Municipio
+     * Obtener Municipio
      * 
-     * Muestra los datos de un municipio especifico
-     * 
-     * 
-     * @param integer $id El ID del municipio a buscar
+     * Obtiene todos los datos de un municipio
+     * @OA\Get (
+     *     path="/municipios/{id}",
+     *     tags={"Municipios"},
+     *    @OA\Parameter(
+     *         name="id",
+     *         required=true,
+     *         in="path",
+     *         description="El id del municipio",
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64",
+     *             minimum=1,
+     *             maximum=165,
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 type="object",
+     *                 property="data",
+     *                 
+     *                       
+     *                       @OA\Property(property="id_municipio",type="number",example="1"),
+     *                       @OA\Property(property="nombre_municipio",type="string",example="Alquizar"),
+     *                       @OA\Property(property="es_cabecera_provincial",type="boolean",example="false"),
+     *                         )
+     *                    
+     *             )
+     *         )
+     *     ),
+     * *     @OA\Response(
+     *         response=404,
+     *         description="Not Found",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 type="array",
+     *                 property="data",
+     *                 @OA\Items(
+     *                     type="object",
+     *                     @OA\Property(
+     *                         property="msg",
+     *                         type="string",
+     *                         example="La provincia con ese id no existe"
+     *                     ),
+     *                 )
+     *             )
+     *         )
+     *     )
+     * )
      */
     public function show($id)
     {
