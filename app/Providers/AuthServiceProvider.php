d209753d3157afd041ec0c6199e0db4d6079dd18
yadidev <yadinds123@gmail.com>
2023-11-11 22:12:06 -0500

    Eliminando scramble

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -24,8 +24,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('viewApiDocs', function () {
-           return true; 
-        });
+        // Gate::define('viewApiDocs', function () {
+        //    return true; 
+        // });
     }
 }
